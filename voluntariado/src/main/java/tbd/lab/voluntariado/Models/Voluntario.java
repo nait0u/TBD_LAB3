33f0f36b7def0b9fb67d6152244b443f0620a26b
Felipe Aravena <hernan.aravena.k@usach.cl>
2023-07-23 19:12:16 -0400

    [Update] Se agrega una lista con las habilidades para un voluntario

--- voluntariado/src/main/java/tbd/lab/voluntariado/Models/Voluntario.java
+++ voluntariado/src/main/java/tbd/lab/voluntariado/Models/Voluntario.java
@@ -3,6 +3,8 @@ package tbd.lab.voluntariado.Models;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Document("voluntario")
 public class Voluntario {
 
@@ -13,97 +15,72 @@ public class Voluntario {
     private String nombre;
     private String password;
     private String atributos;
+    private List<Habilidad> habilidades;
 
-    //CONSTRUCTOR Voluntario
-    public void Voluntario(){
-    }
 
-    //CONSTRUCTOR Voluntario
-    public void Voluntario(long id, String correo, String usuario, String nombre, String password, String atributos){
+    public Voluntario(long id, String correo, String usuario, String nombre, String password, String atributos, List<Habilidad> habilidades) {
         this.id = id;
         this.correo = correo;
         this.usuario = usuario;
         this.nombre = nombre;
         this.password = password;
         this.atributos = atributos;
+        this.habilidades = habilidades;
     }
-
-    //GETTERS Voluntario
-
 
     public long getId() {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getCorreo() {
         return correo;
     }
 
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
 
     public String getUsuario() {
         return usuario;
     }
 
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getAtributos() {
         return atributos;
     }
 
-    //SETTERS Voluntario
-
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
     public void setAtributos(String atributos) {
         this.atributos = atributos;
     }
 
-    //TOSTRING Voluntario
-
-
-    @Override
-    public String toString() {
-        return "Voluntario{" +
-                "id=" + id +
-                ", correo='" + correo + '\'' +
-                ", usuario='" + usuario + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", password='" + "******" + '\'' +
-                ", atributos='" + atributos + '\'' +
-                '}';
+    public List<Habilidad> getHabilidades() {
+        return habilidades;
     }
 
+    public void setHabilidades(List<Habilidad> habilidades) {
+        this.habilidades = habilidades;
+    }
 }
